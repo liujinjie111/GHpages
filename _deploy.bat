e5dd195a4a80655d216f10e5ce134589aaa5dad0
liujinjie111 <jinjie_liu@fas.harvard.edu>
2019-08-17 11:49:51 -0400

    add deploy.bat

---
+++ _deploy.bat
@@ -0,0 +1,30 @@
+git checkout -b gh-pages
+git rm '.gitignore'
+git rm '.nojekyll'
+git rm '.travis.yml'
+git rm '01_chapter.Rmd'
+git rm 'DESCRIPTION'
+git rm 'LICENSE'
+git rm 'README.md'
+git rm '_bookdown.yml'
+git rm '_build.bat'
+git rm '_build.sh'
+git rm '_output.yml'
+git rm 'appveyor.yml'
+git rm 'build.ps1'
+git rm 'build_local.ps1'
+git rm 'deploy.ps1'
+git rm 'deploy_local.ps1'
+git rm 'dss-template.Proj'
+git rm 'images/readme-license.png'
+git rm 'index.Rmd'
+git rm 'preamble.tex'
+git rm 'style.css'
+git rm 'template.gitignore'
+call xcopy .\docs . /E
+git add .
+git commit -m "deploy.bat"
+git push origin gh-pages
+git checkout master
+git push origin master
+git branch -D gh-pages
